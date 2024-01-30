d811a85aef3c4b7819d2701a081abb9bc04ee2a4
Jhonatan <jhonatandaar@gmail.com>
2024-01-29 22:04:44 -0500

    chore: fix day display in calendar

--- src/components/course-calendar.vue
+++ src/components/course-calendar.vue
@@ -10,10 +10,10 @@ const courses = useCourseStore();
 const days = {
   0: "Lunes",
   1: "Martes",
-  3: "Miércoles",
-  4: "Jueves",
-  5: "Viernes",
-  6: "Sábado",
+  2: "Miércoles",
+  3: "Jueves",
+  4: "Viernes",
+  5: "Sábado",
 };
 
 const daySegments = Array.from({ length: 20 }, (_, i) => {
@@ -73,7 +73,7 @@ function isDateBetween(range: { start: string; end: string }, date: string) {
                 </div>
                 <div class="gap-1 flex flex-col w-full p-1">
                   <div
-                    v-for="lecture in lecturesByDay[day]"
+                    v-for="lecture in lecturesByDay[parseInt(day) + 1]"
                     v-bind:key="`${lecture.course_name}-${day}-${segment}`"
                     class="w-full h-full"
                   >
